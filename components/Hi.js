ba72152c28ae5bcfa8644974c3e891bffebcfc2e
Shimil <shimiltharayil007@gmail.com>
2022-12-28 23:27:06 +0530

    Initial commit

---
+++ components/Hi.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Hi = () => {
+  return (
+    <View>
+      <Text className="text-3xl">Hi</Text>
+    </View>
+  )
+}
+
+export default Hi
